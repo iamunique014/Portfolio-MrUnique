cfcf09c0792c8bd9a4831b29b1f67b4c10ba83f6
Alungile Mvenyeza <alungilemvenyeza@gmail.com>
2024-12-06 23:39:00 +0200

    Added ContactViewModel

---
+++ Portfolio-MrUnique/ViewModel/ContactViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Portfolio_MrUnique.ViewModel
+{
+    public class ContactViewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+    }
+}
